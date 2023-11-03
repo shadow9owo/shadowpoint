9ab2b3c4dd1c6029c1a30f056b18ecf6ba564f33
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2023-11-03 16:58:28 +0100

    Add files via upload

---
+++ shadowpoint/shadowpoint/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace shadowpoint
+{
+    public partial class Form1 : Form
+    {
+        private static string selpath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath.ToString();
+                selpath = folderBrowserDialog1.SelectedPath.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selpath != null)
+            {
+                Form2 a = new Form2(selpath);
+                a.ShowDialog();
+            }else
+            {
+                MessageBox.Show("Please select an presentation folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
