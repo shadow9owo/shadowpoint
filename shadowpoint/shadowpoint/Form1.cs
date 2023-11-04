f9ff38202b4533479343e22570349eb85a018764
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2023-11-04 18:19:29 +0100

    Add files via upload

---
+++ shadowpoint/shadowpoint/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace shadowpoint
+{
+    public partial class Form1 : Form
+    {
+        private static string selpath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath.ToString();
+                selpath = folderBrowserDialog1.SelectedPath.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selpath != null)
+            {
+                this.Hide();
+                Form2 a = new Form2(selpath, 1);
+                a.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Please select an presentation folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\"))
+            {
+                MessageBox.Show("please create a project first", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+        }
+    }
+}
---
+++ shadowpoint/shadowpoint/Form3.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace shadowpoint
+{
+    public partial class Form3 : Form
+    {
+        private string selectedtype = "";
+        private List<int> cords = new List<int>();
+        private string presentationname;
+        private string workspace = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\";
+        private int slide = 1;
+        private string[] slidecof;
+
+        public Form3(string presname)
+        {
+            InitializeComponent();
+            presentationname = presname;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\" + presentationname + @"\" + slide))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\" + presentationname + @"\" + slide);
+            }
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\" + presentationname + @"\" + slide + @"\slide.cof"))
+            {
+                slidecof = File.ReadAllLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\" + presentationname + @"\" + slide + @"\slide.cof");
+                int slides;
+                Int32.TryParse(slidecof[0], out slides);
+            }
+        }
+
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {/*
+            cords.Clear();
+            cords.Add(e.X);
+            cords.Add(e.Y);
+            if (selectedtype.ToLower().Contains("text"))
+            {
+                int x, y, size;
+                Color color = System.Drawing.ColorTranslator.FromHtml(tempfile[5]);
+
+                Label a = new Label
+                {
+                    Name = "Label_" + ,
+                    Text = tempfile[4].Replace("\n", Environment.NewLine),
+                    Location = new System.Drawing.Point(x, y),
+                    Font = new System.Drawing.Font("Arial", size),
+                    BackColor = Color.Transparent,
+                    ForeColor = color,
+                    AutoSize = true
+                };
+
+                this.Controls.Add(a);
+            }
+            else if (selectedtype.ToLower().Contains("image"))
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                openFileDialog.Title = "Select PNG File";
+                openFileDialog.Filter = "PNG Files|*.png";
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    File.Copy(filePath, Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\shadowpoint\projects\" + presentationname + @"\" + slide + @"\" + Path.GetFileName(filePath));
+                }
+            }else
+            {
+
+            }
+        */}
+    }
+}
